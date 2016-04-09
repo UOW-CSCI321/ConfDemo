a3045f59a4814fdbc7c35850c0799f8640893431
Matthew Boroczky <matthew.boroczky@gmail.com>
2016-04-09 18:06:02 +1000

    Started doing some location stuff but couldn't get the map view function to be called (should be auto) Tried implementing another method however map info on click is not displayed properly

--- ConfPlus/Explore/EventDetailTableViewController.swift
+++ ConfPlus/Explore/EventDetailTableViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import MapKit
 
-class EventDetailTableViewController: UITableViewController {
+class EventDetailTableViewController: UITableViewController, MKMapViewDelegate {
 
 	@IBOutlet weak var locationMapView: MKMapView!
+    let regionRadius: CLLocationDistance = 1000
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +22,46 @@ class EventDetailTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        locationMapView.delegate = self
+        let lat = 21.282778
+        let long = -157.829444
+        let initialLocation = CLLocation(latitude: lat, longitude: long)
+        centerMapOnLocation(initialLocation)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        annotation.title = "title"
+        annotation.subtitle = "subtitle"
+        locationMapView.addAnnotation(annotation)
     }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        locationMapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    // called for each annotation
+    /*func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        if let annotation = annotation {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) as? MKPinAnnotationView // if avaliable reuse annotation view
+
+            {
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                // use annotation view
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                //view.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as! UIView
+            }
+            return view
+        }
+        return nil
+    }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
