9f5c0fc55cff4e9601b9dfd3ea049c927818f58a
Matthew Boroczky <matthew.boroczky@gmail.com>
2016-04-16 16:07:04 +1000

    Added code to try and do post request to server. making commit as i'm about to alter info.plist

--- ConfPlus/LoginViewController.swift
+++ ConfPlus/LoginViewController.swift
@@ -12,6 +12,8 @@ import Localize_Swift
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginView: UIView!
+    @IBOutlet var usernameTextfield: UITextField!
+    @IBOutlet var passwordTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,79 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func data_request()
+    {
+        let url:NSURL = NSURL(string: "http://b0d1d301.ngrok.io/api/v1")!
+        let session = NSURLSession.sharedSession()
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        
+        let paramString = "method=getUser&email=matt1@test.com"
+        request.HTTPBody = paramString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = session.dataTaskWithRequest(request) {
+            (
+            let data, let response, let error) in
+            
+            guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
+                print("error")
+                return
+            }
+            
+            let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print(dataString)
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+    @IBAction func LoginPressed(sender: AnyObject) {
+        data_request()
+        /*print("LOGIN pressed")
+        let username = usernameTextfield.text
+        let password = passwordTextfield.text
+        
+        let url = NSURL(string: "http://b0d1d301.ngrok.io/api/v1")
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        
+        let postString = "method=getUser&email=\(username)"
+        print(postString)
+        
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                print("error: \(error)")
+                return
+            }
+            
+            //print response obj
+            print("*** response = \(response)")
+            
+            //print response body
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("*** response data \(responseString)")
+            
+            //var err:NSError?
+            let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
+            
+            if let parseJSON = json {
+                var username = parseJSON["username"] as? String
+                print("username: \(username)")
+            }
+            
+            
+        }
+        task.resume()*/
+        
+    }
 
     /*
     // MARK: - Navigation
