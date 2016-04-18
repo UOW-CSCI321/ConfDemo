44f0fb02451591616e4446768152086a07f84a5a
Matthew Boroczky <matthew.boroczky@gmail.com>
2016-04-18 18:01:25 +1000

    tried to change uitableview into tableview. problem was that it is a tableview in storyboard

--- ConfPlus/Explore/EventDetailViewController.swift
+++ ConfPlus/Explore/EventDetailViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class EventDetailTableViewController: UITableViewController, MKMapViewDelegate {
+class EventDetailViewController: UIViewController, UITableViewDelegate, MKMapViewDelegate {
 
+    @IBOutlet var tableview: UITableView!
 	@IBOutlet weak var locationMapView: MKMapView!
     let regionRadius: CLLocationDistance = 1000
 	
@@ -29,8 +30,25 @@ class EventDetailTableViewController: UITableViewController, MKMapViewDelegate {
         //var spanValue = 0.2
         
         //locationMapView.delegate = self
-        let lat = 21.282778
-        let long = -157.829444
+        let lat:CLLocationDegrees = -34.406513
+        let long:CLLocationDegrees = 150.882454
+        /*let coordinate = CLLocationCoordinate2DMake(lat, long)
+        
+        let spanval:CLLocationDegrees = 0.01
+        let span = MKCoordinateSpan(latitudeDelta: spanval, longitudeDelta: spanval)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        locationMapView.setRegion(region, animated: true)
+        
+        let annot = MKPointAnnotation()
+        annot.title = "a"
+        annot.subtitle = "b"
+        annot.coordinate = coordinate;
+        
+        
+        locationMapView.addAnnotation(annot)*/
+        //let lat = 21.282778
+       // let long = -157.829444
         
         let location = CLLocationCoordinate2DMake(lat, long)
         let annotation = MKPointAnnotation()
@@ -43,7 +61,7 @@ class EventDetailTableViewController: UITableViewController, MKMapViewDelegate {
         
         
         //let initialLocation = CLLocation(latitude: lat, longitude: long)
-        centerMapOnLocation(location)
+        //centerMapOnLocation(location)
         
         /*let annotation = MKPointAnnotation()
         annotation.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
@@ -73,7 +91,8 @@ class EventDetailTableViewController: UITableViewController, MKMapViewDelegate {
                 view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
                 view.canShowCallout = true
                 view.calloutOffset = CGPoint(x: -5, y: 5)
-                //view.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as! UIView
+                view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
+                //view.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as UIView
             }
             return view
         }
@@ -84,72 +103,5 @@ class EventDetailTableViewController: UITableViewController, MKMapViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
