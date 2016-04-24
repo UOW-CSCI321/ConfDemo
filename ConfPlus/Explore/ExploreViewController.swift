42a8eafb2ee0d879a72d4b76db0c2eb75479f460
Matthew Boroczky <matthew.boroczky@gmail.com>
2016-04-24 18:26:04 +1000

    Doing post request for a particular event as dummy  - will be multiple events Converting date into readable date

--- ConfPlus/Explore/ExploreViewController.swift
+++ ConfPlus/Explore/ExploreViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ExploreViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        data_request()
 
     }
 
@@ -35,5 +38,62 @@ class ExploreViewController: UIViewController, UITableViewDelegate {
         
         return cell
     }
+    
+    func dateToFullStyleString(dateString:String) -> String
+    { //move into model class for event eventually
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeZone = NSTimeZone(name: "GMT")
+        //dateFormatter.dateStyle = NSDateFormatterStyle.FullStyle
+        
+        let d1 = dateFormatter.dateFromString(dateString)
+        //print(dateStart)
+        
+        let df = NSDateFormatter()
+        df.dateStyle = NSDateFormatterStyle.FullStyle
+        let dstring = df.stringFromDate(d1!)
+        //print(dstring)
+        return dstring
+    }
+    
+    func data_request()
+    {
+        let paramaters = [
+            "method" : "getEvent",
+            "event_id" : "1"
+        ] //at the moment the api call need event id
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if let serverAdd = defaults.stringForKey("server")
+        {
+            Alamofire.request(.POST, serverAdd, parameters: paramaters).responseJSON {
+                response in switch response.result
+                {
+                case .Success:
+                    if let value = response.result.value
+                    {
+                        let json = JSON(value)
+                        let eventName = json["data"][0]["name"].stringValue
+                        //let password = json["data"][0]["password"].stringValue //data[0].password
+                        let eventStartString = self.dateToFullStyleString(json["data"][0]["from_date"].stringValue)
+                        let eventEndString = self.dateToFullStyleString(json["data"][0]["to_date"].stringValue)
+                        let desc = json["data"][0]["description"].stringValue
+                        let posterUrl = json["data"][0]["poster_url"].stringValue
+                        
+                    }
+                case .Failure(let error):
+                    print(error)
+                    
+                }
+                
+            }
+            
+        }else {
+            print("server not set in LoginViewController")
+        }
+        //return password;
+        
+    }
+
 
 }
