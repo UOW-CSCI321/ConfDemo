da24b26843c808b2a510835abdc818f45dcab9d2
Cy Lim <cylim@CYs-Macbook-2015.local>
2016-05-09 11:33:24 +1000

    Add APIServer

---
+++ ConfPlus/APIServer.swift
@@ -0,0 +1,16 @@
+//
+//  APISecret.swift
+//  ConfPlus
+//
+//  Created by CY Lim on 7/05/2016.
+//  Copyright © 2016 Conf+. All rights reserved.
+//
+
+import Foundation
+
+class APIServer{
+	let KEY = "AHWQQPOAEkUoMjMPGep4za0PVaIOFyKt"
+	let SECRET = "KsBg70irVEho4FojGBHa301mlsKut0lD"
+	let URL = "https://6f7a5a2d.ngrok.io/api/v1"
+	//let SALT = "N3v3rEV3r10V3y0V"
+}
