dcbfce87ca8e721728ab1c5f2008cbdd11cee155
Matthew Boroczky <matthew.boroczky@gmail.com>
2016-05-11 14:59:24 +1000

    Fixed up my local cocoa pod installation and installed OGCCryptoHash

---
+++ Pods/Target Support Files/OGCryptoHash/OGCryptoHash-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OGCryptoHash : NSObject
+@end
+@implementation PodsDummy_OGCryptoHash
+@end
---
+++ Pods/Target Support Files/OGCryptoHash/OGCryptoHash-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "NSString+OGCryptoHash.h"
+
+FOUNDATION_EXPORT double OGCryptoHashVersionNumber;
+FOUNDATION_EXPORT const unsigned char OGCryptoHashVersionString[];
+
