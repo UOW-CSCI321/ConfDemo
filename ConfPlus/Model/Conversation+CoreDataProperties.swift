7f32158df5b8899ea5a9b0b66a15aeba95479ec8
Matthew Boroczky <matthew.boroczky@gmail.com>
2016-05-14 23:24:22 +1000

    Created new model files and deleted participants model

--- ConfPlus/Model/Conversation+CoreDataProperties.swift
+++ ConfPlus/Model/Conversation+CoreDataProperties.swift
@@ -1,9 +1,9 @@
 //
 //  Conversation+CoreDataProperties.swift
-//  ConfPlus
+//  
 //
-//  Created by CY Lim on 8/05/2016.
-//  Copyright © 2016 Conf+. All rights reserved.
+//  Created by Matthew Boroczky on 14/05/2016.
+//
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
@@ -17,6 +17,6 @@ extension Conversation {
     @NSManaged var conversation_id: NSNumber?
     @NSManaged var name: String?
     @NSManaged var messages: NSSet?
-    @NSManaged var participants: NSSet?
+    @NSManaged var users: NSSet?
 
 }
