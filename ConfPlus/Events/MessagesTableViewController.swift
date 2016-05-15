440b46b9d0e4a75bbb0474c121b2de412e24c817
Matthew Boroczky <matthew.boroczky@gmail.com>
2016-05-15 23:58:51 +1000

    setup message bubble colours and images

--- ConfPlus/Events/MessagesTableViewController.swift
+++ ConfPlus/Events/MessagesTableViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import MPGNotification
 
+
 class MessagesTableViewController: UIViewController {
     
     @IBOutlet var conversationTable: UITableView!
@@ -63,10 +64,9 @@ class MessagesTableViewController: UIViewController {
         messengerVC.title = userConversations[indexPath.row].name
         messengerVC.senderDisplayName = userConversations[indexPath.row].lastmsg_email
         self.hidesBottomBarWhenPushed = true //need to hide tab bar to show message bar at the bottom. i tried to move message bar in JSQMessagesViewController but it has some action on it that when clicked it will move back down
-        
-//        let eventVC:EventDetailTableViewController = segue.destinationViewController as! EventDetailTableViewController
-//        eventVC.event = events[indexPath.row]
     }
+    
+
 	
 }
 
--- ConfPlus/Events/MessengerViewController.swift
+++ ConfPlus/Events/MessengerViewController.swift
@@ -13,12 +13,15 @@ import JSQMessagesViewController
 
 class MessengerViewController: JSQMessagesViewController {
     
-    var messages = [[Message]]()
+    var messages = [JSQMessage]()
     var conversationID = ""
+    var outgoingBubbleImageView: JSQMessagesBubbleImage!
+    var incomingBubbleImageView: JSQMessagesBubbleImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("conversationid: \(conversationID)")
+        setupBubbles()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -26,5 +29,38 @@ class MessengerViewController: JSQMessagesViewController {
 
     }
     
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!,
+                                 messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    private func setupBubbles() {
+        let factory = JSQMessagesBubbleImageFactory()
+        outgoingBubbleImageView = factory.outgoingMessagesBubbleImageWithColor(
+            UIColor.jsq_messageBubbleBlueColor())
+        incomingBubbleImageView = factory.incomingMessagesBubbleImageWithColor(
+            UIColor.jsq_messageBubbleLightGrayColor())
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!,
+                                 messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item]
+        if message.senderId == senderId { //check if message was sent by local user
+            return outgoingBubbleImageView
+        } else { // if not local user return incoming message
+            return incomingBubbleImageView
+        }
+    }
+    
+//    override func collectionView(collectionView: JSQMessagesCollectionView!,
+//                                 avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+//        return nil
+//    }
 }
 
