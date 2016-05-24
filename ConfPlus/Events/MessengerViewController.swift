f666c313671f647533d6495a0fcd3c48dedb54d9
Matthew Boroczky <matthew.boroczky@gmail.com>
2016-05-24 16:21:31 +1000

    cleaned up and verified still working

--- ConfPlus/Events/MessengerViewController.swift
+++ ConfPlus/Events/MessengerViewController.swift
@@ -248,10 +248,7 @@ class MessengerViewController: JSQMessagesViewController {
     {
         if isDispatchEmpty {
             isDispatchEmpty = false
-            //let notification = MPGNotification(title: "Updating", subtitle: "it might takes some time for updating.", backgroundColor: UIColor.orangeColor(), iconImage: nil)
-            //notification.show()
             
-            //sendMessage(email:String, content:String, conversationID:String, completion:
             APIManager().sendMessage(email, content: content, conversationID: conversationID){ result in
                 if result == true
                 {
@@ -269,27 +266,6 @@ class MessengerViewController: JSQMessagesViewController {
                 }
                 self.isDispatchEmpty = true
                 self.finishSendingMessage()
-                //change colour back
-                
-                
-//                result in
-//                dispatch_async(dispatch_get_main_queue()) {
-//
-//                    //then add the message to dbs - dont think this is needed anymore as the api will call get messages eventually and get this, it will still be displayed
-//                   // self.handler.addNewMessage(json["data"][i])
-//                    //notification.hidden = true
-//                    self.isDispatchEmpty = true
-//                    //self.databaseMessages = ModelHandler().getMessageForConversation(self.conversation)!
-//                    //print("MESSAGE COUNT: \(self.databaseMessages.count)")
-//                    //self.messagesToJSQMessages()
-//                    //                    self.events = ModelHandler().getEvents("0")
-//                    //                    self.EventsTableView.reloadData()
-//                    
-//                    //                    if self.refresher.refreshing {
-//                    //                        self.refresher.endRefreshing()
-//                    //                    }
-//                
-//                }
             }
         }
 
@@ -301,14 +277,7 @@ class MessengerViewController: JSQMessagesViewController {
         addMessage(senderId, displayName: senderDisplayName, date: date, text: text)
         //send the message we just added to the JSQMessage array
         sendMessageByAPI(senderId, conversation_id: self.conversationID, content: text)
-        
-        //else
-            //change bubble colour
-        //finish sending message
-        
-        //JSQSystemSoundPlayer.jsq_playMessageSentSound()
 
-        //finishSendingMessage()
     }
     
     
