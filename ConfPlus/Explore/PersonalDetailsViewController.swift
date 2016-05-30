b0d0d89fa1e5812345ac3a3b4a8b0709a406fe36
CY Lim <cylim@cy.local>
2016-05-30 14:19:25 +1000

    Fix: Personal Details View able to assign info to data

--- ConfPlus/Explore/PersonalDetailsViewController.swift
+++ ConfPlus/Explore/PersonalDetailsViewController.swift
@@ -7,22 +7,52 @@
 //
 
 import UIKit
+import PKHUD
 
 class PersonalDetailsViewController: UIViewController {
 	
-	var TOTAL_TICKET_QUANTITY:Int = 1
-	var ticketName = [String]()
-	let type = ["name", "email"]
+	@IBOutlet weak var tableView: UITableView!
+	
+	var tickets = [Coupon]()
+	let type = ["Name", "Email"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 		
     }
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "goToConfirmtion" {
+			for section in 0..<tableView.numberOfSections{
+				for row in 0..<tableView.numberOfRowsInSection(section){
+					let cell:PersonalDetailsTableViewCell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: row, inSection: section)) as! PersonalDetailsTableViewCell
+					if cell.detailType.text == "Name" {
+						guard let name = cell.typeResponseTextField.text else {
+							HUD.show(.Label("Name must not be empty"))
+							return
+						}
+						tickets[section].name = name
+					} else if cell.detailType.text == "Email" {
+						guard let email = cell.typeResponseTextField.text else {
+							HUD.show(.Label("Email must not be empty"))
+							return
+						}
+						tickets[section].email = email
+					}
+					
+				}
+			}
+			
+			let vc = segue.destinationViewController as! PaymentViewController
+			vc.tickets = tickets
+		}
+	}
+	
 }
 
 extension PersonalDetailsViewController: UITableViewDelegate {
 	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-		return TOTAL_TICKET_QUANTITY
+		return tickets.count
 	}
 	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,7 +60,7 @@ extension PersonalDetailsViewController: UITableViewDelegate {
 	}
 	
 	func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-		return "\(ticketName[section % ticketName.count])-\(section)"
+		return tickets[section].ticket[0].name
 	}
 	
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -42,4 +72,5 @@ extension PersonalDetailsViewController: UITableViewDelegate {
 		
 		return cell
 	}
+	
 }
