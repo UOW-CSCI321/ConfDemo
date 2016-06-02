b66f9424dd41dc5c2b0e494246d1bba899ba4b96
Matthew Boroczky <matthew.boroczky@gmail.com>
2016-06-02 21:17:59 +1000

    ticket profile details modified. details not display for some reason

--- ConfPlus/Events/TicketProfileTableViewController.swift
+++ ConfPlus/Events/TicketProfileTableViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  TicketProfileTableViewController.swift
 //  confDemo
@@ -14,8 +15,11 @@ class TicketProfileTableViewController: UITableViewController {
 	
 	@IBOutlet weak var qrImage: UIImageView!
 	
+    @IBOutlet weak var titleDetailsLabel: UILabel!
+    @IBOutlet weak var venueDetailsLabel: UILabel!
 	@IBOutlet weak var roomDetailLabel: UILabel!
 	@IBOutlet weak var typeDetailLabel: UILabel!
+    @IBOutlet weak var classDetailsLabel: UILabel!
 	@IBOutlet weak var seatDetailLabel: UILabel!
 	@IBOutlet weak var nameDetailLabel: UILabel!
 	@IBOutlet weak var emailDetailLabel: UILabel!
@@ -30,10 +34,20 @@ class TicketProfileTableViewController: UITableViewController {
 		if ticket != nil{
 			generateQR()
 			
-			roomDetailLabel.text = ticket?.room_name
+            var title:String = (ticket?.title)!
+            title += " - "
+            title += (ticket?.ticket_name)!
+            titleDetailsLabel.text = title
+            
+            venueDetailsLabel.text = ticket?.venue_id
+            roomDetailLabel.text = ticket?.room_name
 			typeDetailLabel.text = ticket?.type
-			seatDetailLabel.text = String(ticket?.seat_num)
-			nameDetailLabel.text = user.stringForKey("firstName")
+            classDetailsLabel.text = ticket?.ticket_class
+			seatDetailLabel.text = ticket?.seat_num
+            var name:String = user.stringForKey("firstName")!
+            name += " "
+            name += user.stringForKey("lastName")!
+            nameDetailLabel.text = name
 			emailDetailLabel.text = user.stringForKey("email")
 			
 		}
